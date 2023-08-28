0ff6e8a7038a319efba11aadaf899b2b2fc4c83e
SahejChandok <chandoksahej@gmail.com>
2023-08-27 21:55:07 -0400

    deployment code

---
+++ eks.py
@@ -0,0 +1,54 @@
+from kubernetes import client, config
+
+#load kubernetes configuration
+config.load_kube_config()
+
+# create a kubernetes api client
+api_client = client.ApiClient()
+
+deployment = client.V1Deployment(
+    metadata=client.V1ObjectMeta(name="my-flask-app"),
+    spec=client.V1DeploymentSpec(
+        replicas=1,
+        selector=client.V1LabelSelector(
+            match_labels={"app": "my-flask-app"}
+        ),
+        template=client.V1PodTemplateSpec(
+            metadata=client.V1ObjectMeta(
+                labels={"app": "my-flask-app"}
+            ),
+            spec=client.V1PodSpec(
+                containers=[
+                    client.V1Container(
+                        name="my-flask-container",
+                        image="593254798245.dkr.ecr.us-west-2.amazonaws.com/my_cloud_native_repo:latest",
+                        ports=[client.V1ContainerPort(container_port=5000)]
+                    )
+                ]
+            )
+        )
+    )
+)
+
+# Create the deployment
+api_instance = client.AppsV1Api(api_client)
+api_instance.create_namespaced_deployment(
+    namespace="default",
+    body=deployment
+)
+
+# define a service
+service = client.V1Service(
+    metadata=client.V1ObjectMeta(name="my-flask-service"),
+    spec=client.V1ServiceSpec(
+        selector={"app": "my-flask-app"},
+        ports=[client.V1ServicePort(port=5000)]
+    )
+)
+
+# create a service
+api_instance = client.CoreV1Api(api_client)
+api_instance.create_namespaced_service(
+    namespace="default",
+    body=service
+)
